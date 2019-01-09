529cd94f544e7a8ef85e61f00e13b9123cbd0545
tesh254 <ewachira254@gmail.com>
2019-01-09 15:11:08 +0300

    chore(mongodb setup) - Added mongodb to the api

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const chalk = require('chalk');
+
+const Config  = require('./config/config');
 
 // Set up the express app
 const app = express();
@@ -10,6 +15,16 @@ app.use(logger('dev'));
 
 // Register the port to the server
 app.set('port', process.env.PORT || 8858);
+
+// Setup mongoose database
+mongoose.connect(Config.mongo.url);
+// Set the promise
+mongoose.Promise = Promise;
+// Console successfull
+mongoose.connection.on('open', (err) => {
+    if (err) console.log(chalk.red("Database not connected"))
+    console.log(chalk.green("Database connected"))
+});
 
 // Parse incoming requests
 app.use(bodyParser.json());
---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongo:{
+        url: process.env.DATABASE_URL
+    }
+}
